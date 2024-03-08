ef7d9038a47a6d6761486723c869aa69598d96ee
xk-rl <bledion.morina@hotmail.com>
2024-03-08 19:54:31 +0100

    added the option to choose the hard drive and little help on the kernel selection

--- airootfs/usr/local/bin/installer.py
+++ airootfs/usr/local/bin/installer.py
@@ -11,13 +11,9 @@ passw = ""
 #Create window class
 installer = ThemedTk(theme="breeze")
 
-#Print to terminal
-linux.system("echo 'Starting the CLD installer'")
-
 #Get user and password
 def GetUserPass():
     global user, passw
-    linux.system("echo Getting user and password")
     for widget in installer.winfo_children():               #Clear all widgets
         widget.destroy()
     user = ntk.StringVar()
@@ -30,16 +26,31 @@ def GetUserPass():
     tk.Button(installer, text="Next", command=GetKernel).place(relx=0.5, rely=0.6, anchor="center")
 
 def GetKernel():
-    global user, passw
-    print("echo Received user and password as " + user.get() + " and " + passw.get())
     for widget in installer.winfo_children():               #Clear all widgets
         widget.destroy()
     tk.Label(installer, text="Select kernel", font=("Helvetica", 40)).place(relx=0.5, rely=0.1, anchor="center")
-    tk.Button(installer, text="Linux", command=GetKernel).place(relx=0.5, rely=0.2, anchor="center")
-    tk.Button(installer, text="Linux-LTS", command=GetKernel).place(relx=0.5, rely=0.3, anchor="center")
-    tk.Button(installer, text="Linux-Zen", command=GetKernel).place(relx=0.5, rely=0.4, anchor="center")
+    tk.Button(installer, text="Linux (Recommended)", command=GetDrives).place(relx=0.5, rely=0.2, anchor="center")
+    tk.Button(installer, text="Linux-LTS", command=GetDrives).place(relx=0.5, rely=0.3, anchor="center")
+    tk.Button(installer, text="Linux-Zen", command=GetDrives).place(relx=0.5, rely=0.4, anchor="center")
+    tk.Label(installer, text="Not sure what to pick?", font=("Helvetica", 20)).place(relx=0.5, rely=0.5, anchor="center")
+    tk.Label(installer, text="Linux - Latest linux kernel", font=("Helvetica", 10)).place(relx=0.5, rely=0.6, anchor="center")
+    tk.Label(installer, text="Linux LTS - Long Term Support kernel", font=("Helvetica", 10)).place(relx=0.5, rely=0.7, anchor="center")
+    tk.Label(installer, text="Linux Zen - Hacker tested kernel", font=("Helvetica", 10)).place(relx=0.5, rely=0.8, anchor="center")
 
-#Welcome screen
+def GetDrives():
+    drives = linux.popen("lsblk --nodeps --output NAME | grep -v 'NAME'").read()
+    for widget in installer.winfo_children():               #Clear all widgets
+        widget.destroy()
+    drives = drives.split("\n")
+
+    tk.Label(installer, text="Select drive, WARNING: ALL DATA ON DRIVE WILL BE LOST", font=("Helvetica", 20)).place(relx=0.5, rely=0.1, anchor="center")
+    tk.Label(installer, text="Drives:", font=("Helvetica", 20)).place(relx=0.5, rely=0.2, anchor="center")
+
+    for drive in drives:
+        if drive != "":
+            tk.Radiobutton(installer, text=drive, value=drive).place(relx=0.5, rely=0.3+(drives.index(drive)*0.1), anchor="center")
+
+#Welcome
 def Welcome():
                                                             #Create a Welcome frame
     installer.wm_title("Welcome")
