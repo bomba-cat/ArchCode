66598620eba0a8351423e427698acea1fc7c0bba
arch <bledion.morina@hotmail.com>
2024-03-12 21:40:40 +0100

    finished gui for installer and the partitioning commands for the installer

--- airootfs/usr/local/bin/installer.py
+++ airootfs/usr/local/bin/installer.py
@@ -7,15 +7,40 @@ import os as linux
 #Variables
 user = ""
 passw = ""
+sudo = False
 kernel = ""
 drive = ""
+
+#Commands
+packages = [
+    "parted"
+]
+
+commands = [
+    "timedatectl"
+    "mkdir /mnt/boot"
+    #Setup partitions for arch linux: EFI, SWAP and ROOT using ext4 fs
+    f"parted {drive} mklabel gpt"
+    f"parted {drive} mkpart primary fat32 1MiB 300MiB"
+    f"parted {drive} set 1 boot on"
+    f"parted {drive} mkpart primary linux-swap 300MiB 3GiB"
+    f"parted {drive} mkpart primary ext4 3GiB 100%"
+    f"mkfs.fat -F32 {drive}1"
+    f"mkswap {drive}2"
+    f"swapon {drive}2"
+    f"mkfs.ext4 {drive}3"
+    f"mount {drive}3 /mnt"
+    f"mkdir /mnt/boot"
+    f"mount {drive}1 /mnt/boot"
+    f"pacstrap /mnt base linux linux-firmware"
+]
 
 #Create window class
 installer = ThemedTk(theme="breeze")
 
 #Get user and password
 def GetUserPass():
-    global user, passw
+    global user, passw, sudo
     for widget in installer.winfo_children():               #Clear all widgets
         widget.destroy()
     user = ntk.StringVar()
@@ -25,7 +50,8 @@ def GetUserPass():
     tk.Entry(installer, textvariable=user, font=("Courier New", 20)).place(relx=0.5, rely=0.3, anchor="center")
     tk.Label(installer, text="Enter password:", font=("Courier New", 20)).place(relx=0.5, rely=0.4, anchor="center")
     tk.Entry(installer, textvariable=passw, show="*", font=("Courier New", 20)).place(relx=0.5, rely=0.5, anchor="center")
-    tk.Button(installer, text="Next", command=GetKernel).place(relx=0.5, rely=0.6, anchor="center")
+    tk.Checkbutton(installer, text="Root", variable=sudo).place(relx=0.5, rely=0.6, anchor="center")
+    tk.Button(installer, text="Next", command=GetKernel).place(relx=0.5, rely=0.7, anchor="center")
 
 def GetKernel():
     global kernel
@@ -54,6 +80,12 @@ def GetDrives(chosenKernel):
     for d in drives:
         if d != "":
             tk.Radiobutton(installer, text=d, value=d, variable=drive).place(relx=0.5, rely=0.3+(drives.index(d)*0.1), anchor="center")
+
+def Installing():
+    global commands, packages
+    #Label saying installing
+    tk.Label(installer, text="Installing", font=("Courier New", 20)).place(relx=0.5, rely=0.5, anchor="center")
+
 
 #Welcome
 def Welcome():
