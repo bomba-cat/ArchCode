9c74d57d06f2ec559aa487bfd9e1e229204eab68
arch <bledion.morina@hotmail.com>
2024-03-16 13:12:24 +0100

    moves installer to other repo

--- profiledef.sh
+++ profiledef.sh
@@ -20,6 +20,7 @@ file_permissions=(
   ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
+  ["/home/ArchCode/Desktop/Install"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
